89e183e2a2521d7ad7e22326578dcd1a51d4a29f
sanith shetty <sanithrshetty15@gmail.com>
2026-03-22 13:40:13 +0000

    fix: add sys.path insert at top of main.py

--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,6 @@
+import sys
+import os
+sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
 from fastapi import FastAPI, Depends, HTTPException, status, File, UploadFile, Form, Response
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
